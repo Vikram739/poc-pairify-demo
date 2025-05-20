f5d84846fd33db4b1784d3b4ec2c24e7905b458d
Vikram Markali <99202936+Vikram739@users.noreply.github.com>
2025-05-20 07:16:07 +0530

    Update use-took.tsx

--- src/hooks/use-took.tsx
+++ src/hooks/use-took.tsx
@@ -5,15 +5,5 @@ const MOBILE_BREAKPOINT = 768
 export function useTookMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
   return !!isMobile
 }
